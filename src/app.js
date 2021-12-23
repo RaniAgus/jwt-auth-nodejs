a8f0ba6f8db5eefbfa847b1c1ad3cdc0cdeac356
Agustin Ranieri <aguseranieri@gmail.com>
2021-12-23 18:45:42 -0300

    added token verification

--- src/app.js
+++ src/app.js
@@ -10,9 +10,14 @@ app.get('/api', (req, res) => {
   });
 });
 
-app.post('/api/posts', (req, res) => {
-  res.json({
-    message: 'Post created...'
+app.post('/api/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, env.secretkey, (error, authData) => {
+    if (error) {
+      res.sendStatus(401); // Unauthorized
+      return;
+    }
+
+    res.json({ message: 'Post created...', authData });
   });
 });
 
@@ -24,9 +29,46 @@ app.post('/api/login', (req, res) => {
     email: 'agus@rani.com'
   };
 
-  jwt.sign({ user }, env.secretkey, (error, token) => {
-    res.json({ token });
-  });
+  jwt.sign(
+    { user }, 
+    env.secretkey, 
+    (error, token) => {
+      if (error) {
+        res.sendStatus(500); // Internal server error
+      }
+
+      res.json({ token });
+    }
+  );
+
 });
+
+// TOKEN FORMAT
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+
+  // Check if bearer is undefined
+  if (typeof(bearerHeader) === 'undefined') {
+    res.sendStatus(401); // Unauthorized
+    return;
+  }
+
+  // Split at the space and get token from array
+  const [bearer, token] = bearerHeader.split(' ');
+  if (bearer !== 'Bearer') {
+    res.sendStatus(401); // Unauthorized
+    return;
+  }
+
+  // Set the token
+  req.token = token;
+
+  // Next middleware
+  next();
+}
 
 app.listen(5000, () => console.log('Server started at http://localhost:5000/'));
